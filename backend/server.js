d1f9446f800c10dcc0582bc441782b24eb0f5533
AtharvaGGourshete <goursheteatharva@gmail.com>
2025-02-01 20:23:41 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,13 @@ import mongoose from "mongoose";
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
+dotenv.config();
 // MongoDB Connection
 mongoose
-  .connect(
-    process.env.MONGO_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
